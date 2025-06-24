7f00d12a25a09fb7e83178af95f391c796e2069d
Nawaf Kamal <xp.mo91@gmail.com>
2025-06-24 14:22:19 +0300

    finish the lab

---
+++ todo.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def readfile():
+    file = open("todo.txt",'r',encoding='utf-8')
+    return file.read()
+
+def writefile(title):
+    file = open("todo.txt",'a+',encoding='utf-8')
+    file.write(title + '\n')
+
+x = 0
+
+while True:
+    question = input("do you want to add a new To-DO item y for yes, n for no? ")
+    if question == 'y':
+        title = input("Enter the title\n: ")
+        x = x + 1
+        towrite = f'{x}- {title} - {date} - Not Done'
+        writefile(towrite)
+    elif question == 'n':
+        question2 = input("do you want to list your To-Do items y for yes, n for no?  ? ")
+        if question2 == 'y':
+            print('\n',readfile())
+        elif question2 == 'n':
+            pass
+        elif question2 == 'exit':
+            print("thank you for using the To-Do program, come back again soon")
+            break
+    elif question == 'exit':
+        print("thank you for using the To-Do program, come back again soon")
+        break
